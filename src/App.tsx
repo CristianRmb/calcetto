e8332a75d9e6d5ab10c22fa2633007130ed9fb42
Cristian Rimbu <cristian.rimbu@devinterface.com>
2024-10-10 11:01:37 +0200

    Add header to App component using MUI Stack

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import { useState } from 'react';
-
 import './App.css';
+import { Stack } from '@mui/material';
 
 function App() {
-	return <></>;
+	return (
+		<>
+			<Stack>
+				<h1>React App</h1>
+			</Stack>
+		</>
+	);
 }
 
 export default App;
